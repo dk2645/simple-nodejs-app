543e31b744336664cf7f9b47f275600bfaf10f32
Dheeraj kumar <70697789+dk2645@users.noreply.github.com>
2023-09-28 11:47:20 +0530

    Update nodejs.sh

--- nodejs.sh
+++ nodejs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "=========== Push Docker Image to Registry: Start =============="
-sudo apt update -y 
-apt install nodejs -y 
-apt install npm -y 
+sudo apt-get update -y 
+apt-get install nodejs -y 
+apt-get install npm -y 
 node -v
